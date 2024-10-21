b62b57177097b5b8759978de2e3be3f10f3dc19a
Stig Norland <stig.norland@gmail.com>
2024-10-21 09:36:04 +0200

    fixed signed date

--- preferences-commons/src/main/java/no/sikt/nva/person/preferences/commons/model/LicenseInfo.java
+++ preferences-commons/src/main/java/no/sikt/nva/person/preferences/commons/model/LicenseInfo.java
@@ -3,8 +3,16 @@ package no.sikt.nva.person.preferences.commons.model;
 import java.net.URI;
 import java.time.Instant;
 
+import static java.util.Objects.nonNull;
+
 public record LicenseInfo(
         Instant signed,
         URI licenseUri
 ) {
+
+    @Override
+    public Instant signed() {
+        // in order to verify users' licenses, we need to compare the signed date with the current license date
+        return nonNull(signed) ? signed : Instant.MIN;
+    }
 }
